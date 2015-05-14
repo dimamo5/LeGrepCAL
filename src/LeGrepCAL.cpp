6570e0ed918fd9f748fe41fb8f1b0b5e47898ad0
Diogo Moura <diogomoura13@gmail.com>
2015-05-14 17:30:45 +0100

    Inicilizacao do projecto

---
+++ src/LeGrepCAL.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : LeGrepCAL.cpp
+// Author      : Diogo Moura
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
