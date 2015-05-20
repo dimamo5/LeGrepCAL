ad2de162179a82b6e5bc71a74b30592981a0065a
Diogo Moura <diogomoura13@gmail.com>
2015-05-20 15:34:07 +0100

    Boyer Moore implementado

---
+++ src/BoyerMoore.h
@@ -0,0 +1,36 @@
+/*
+ * BoyerMoore.h
+ *
+ *  Created on: 20/05/2015
+ *      Author: Utilizador
+ */
+
+#ifndef BOYERMOORE_H_
+#define BOYERMOORE_H_
+
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class BoyerMoore {
+private:
+
+	int const ALPHABET_SIZE = 256;
+	string haystack;
+	string needle;
+
+	vector<int> makeCharTable(string needle);
+	vector<int> makeOffsetTable(string needle);
+	bool isPrefix(string needle, int p);
+	int suffixLength(string needle, int p);
+
+public:
+	BoyerMoore(string haystack, string needle);
+	int indexOf(string haystack, string needle);
+	const string& getHaystack() const;
+	const string& getNeedle() const;
+};
+
+#endif /* BOYERMOORE_H_ */
--- src/LeGrepCAL.cpp
+++ src/LeGrepCAL.cpp
@@ -7,9 +7,24 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
+#include "BoyerMoore.h"
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "BoyerMoore" << endl; // prints !!!Hello World!!!
+
+	ifstream ficheiro;
+	ficheiro.open("resources/1.txt");
+	string haystack((std::istreambuf_iterator<char>(ficheiro)), (std::istreambuf_iterator<char>()));
+
+	cout << haystack << endl;
+
+	BoyerMoore teste(haystack, "Name");
+
+	cout << "Index:" << teste.indexOf(teste.getHaystack(), teste.getNeedle());
+	cout<<haystack[135]<<haystack[136]<<haystack[137]<<haystack[138];
+
 	return 0;
 }
+
